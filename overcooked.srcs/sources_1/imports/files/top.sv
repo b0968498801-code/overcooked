268318b6dc031fcbebe01bdbdae2655da91a3b12
chamber0328-rgb <chamber0328@gmail.com>
2026-05-28 00:34:09 +0800

    你的修改說明

--- overcooked.srcs/sources_1/imports/files/top.sv
+++ overcooked.srcs/sources_1/imports/files/top.sv
@@ -1,6 +1,7 @@
 // Top Level Module - Overcooked on Basys3
 // Connects all submodules
 // 11111111
+// 22222222
 module top (
     input  logic        clk,            // 100MHz
     input  logic        btnC,           // start
